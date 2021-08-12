f819bd085d7fb3113d2a0aba5e6804e8fdd1919a
BlackCat <44997646+Chatyusha@users.noreply.github.com>
2021-08-12 12:27:52 +0900

    Update ubuntu.sh

--- installer/ubuntu.sh
+++ installer/ubuntu.sh
@@ -10,6 +10,6 @@ curl -OL http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar xvf install-tl-unx.tar.gz
 rm *.tar.gz
 cd install-tl-*
-echo "I" | ./install-tl -no-gui -repository http://mirror.ctan.org/systems/texlive/tlnet/
+echo "I" | ./install-tl -no-gui
 
 ls /usr/local/texlive
